71925439a6e93171dd8a50530265218e92a62af7
ctaylor0002 <charles4312@comcast.net>
2022-08-21 18:52:32 -0400

    Added the first step of creating players in the Game class.

--- game.py
+++ game.py
@@ -1,8 +1,17 @@
+from human import Human
+from bot import Bot
+from player import Player
+
 
 class Game:
 
     def __init__(self, player_count):
         self.player_count = player_count
 
-    
+    def create_player_objects(self):
+        if self.player_count == 1:
+            player1_name = input("What is your name? ")
+            
+            player1 = Human(Player(player1_name))
+            print(f"Welcome {player1.player_name}!")
     
