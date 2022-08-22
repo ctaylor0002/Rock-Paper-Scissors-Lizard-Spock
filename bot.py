f8aac1527efcfa1a6dcbbd1a27d0c5918fd4dce5
Charles Taylor <ctaylor@dffcorp.com>
2022-08-22 12:25:01 -0400

    Removed the player_type field from the Human and Bot classes. It was a feature that was not used.

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import random
 class Bot(Player):
     
     def __init__(self):
-        self.player_type = 'bot'
         random_names = ["Roberto", "Gregory", "Robin", "Timmy", 'Ricardo', 'Judy']
         name_index = random.randrange(0,len(random_names))
         player_name = random_names[name_index]
--- human.py
+++ human.py
@@ -3,5 +3,4 @@ from player import Player
 class Human(Player):
 
     def __init__(self, player_name):
-        self.player_type = 'human'
         super().__init__(player_name)
